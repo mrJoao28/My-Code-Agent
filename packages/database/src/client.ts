ebadaab8a1b812d89c73e70bf381faca345254ac
Joao Felipe <joaofelipeazevedo28@gmail.com>
2026-07-28 16:39:00 -0400

    Fixing errors

--- packages/database/src/client.ts
+++ packages/database/src/client.ts
@@ -1,13 +1,16 @@
-import "dotenv/config"
+import path from "path"
+import dotenv from "dotenv"
 import { PrismaPg } from "@prisma/adapter-pg"
 import { PrismaClient } from "../generated/prisma/client"
 
+dotenv.config({ path: path.resolve(__dirname, "..", ".env") })
+
 const databaseUrl = process.env.DATABASE_URL
 
-if (!databaseUrl){
+if (!databaseUrl) {
     throw new Error("DATABASE_URL is not set")
 }
 
-const adapter = new PrismaPg({connectionString:databaseUrl})
+const adapter = new PrismaPg({ connectionString: databaseUrl })
 
-export const db = new PrismaClient({adapter})
+export const db = new PrismaClient({ adapter })
